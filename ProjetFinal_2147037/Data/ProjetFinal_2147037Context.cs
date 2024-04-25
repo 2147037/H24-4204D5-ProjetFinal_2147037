765dc98feb6119125546a0269b66462ae37df943
Jin <2147037@cegepmontpetit.ca>
2024-04-25 13:22:25 -0400

    FCT Création du projet

---
+++ ProjetFinal_2147037/Data/ProjetFinal_2147037Context.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjetFinal_2147037.Data
+{
+    public partial class ProjetFinal_2147037Context : DbContext
+    {
+        public ProjetFinal_2147037Context()
+        {
+        }
+
+        public ProjetFinal_2147037Context(DbContextOptions<ProjetFinal_2147037Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ProjetFinal_2147037");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ProjetFinal_2147037/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetFinal_2147037.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ProjetFinal_2147037Context>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProjetFinal_2147037")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
